88adb3fc0739443c18c495059173dd2532db7cf3
mr7kitkat <mr7kitkat@gmail.com>
2025-01-20 23:25:35 +0530

    make some ui enhancements

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,3 +1,15 @@
 
 import "../styles/style.css";
 
+// DOM MODELS
+
+// navigation
+const HAM_MENU = document.querySelector("#theHam");
+HAM_MENU.addEventListener("click", (e) => {
+    HAM_MENU.classList.toggle("active");
+})
+
+class PageManager {
+    constructor() {
+    }
+}
